268cc3dc932fd9b4eda2497710ec4771271a960c
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 <Niyabi>
2021-09-13 19:47:23 +0200

    Added basic scss files

--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,23 @@ module.exports = {
     mode: mode,
 
     entry: {
+        //output filename: path/to/source/file
+        //theme js files
         theme_js_index: path.resolve(__dirname, "src/js/index.js"),
+        //theme critical js files
+        theme_js_critical_index: path.resolve(__dirname, "src/js/index.js"),
+        //theme css files
         theme_css_index: path.resolve(__dirname, "src/styles/index.scss"),
+        theme_css_blog: path.resolve(__dirname, "src/styles/blog.scss"),
+        theme_css_post: path.resolve(__dirname, "src/styles/post.scss"),
+        //theme critical css files
+        theme_css_critical_index: path.resolve(__dirname, "src/styles/critical_styles/critical_index.scss"),
+        theme_css_critical_blog: path.resolve(__dirname, "src/styles/critical_styles/critical_blog.scss"),
+        theme_css_critical_post: path.resolve(__dirname, "src/styles/critical_styles/critical_post.scss"),
     },
 
     output: {
+        //output directory
         path: path.resolve(__dirname, "public"),
         filename: 'js/[name].js',
         assetModuleFilename: "images/[hash][ext][query]",
@@ -52,6 +64,7 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
+            //output css files to styles folder
             filename: 'styles/[name].css'
         }),
         new IgnoreEmitPlugin(/theme_css.*\.js$/)
